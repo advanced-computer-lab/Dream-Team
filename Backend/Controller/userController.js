1a500c4fcb66a9ddd92fdbf2eb1726d21d4b580d
Nadeen Sayed <nadeensayedd@yahoo.com>
2021-11-07 20:46:30 +0200

    Edit and create components
    final draft after messing it up

---
+++ Backend/Controller/userController.js
@@ -0,0 +1,132 @@
+const Flight=require('../model/flight');
+
+
+
+const createFlight=(req,res)=>
+{
+    console.log('request came');
+    console.log(req.body);
+    const flight=new Flight(
+        {
+            flight_number: req.body.flight_number,
+            flight_date:req.body.flight_date,
+            from:req.body.from,
+            to: req.body.to,
+            departure_time : req.body.departure_time,
+            arrival_time: req.body.arrival_time,
+            economy_seats_available:req.body.economy_seats_available,
+            business_seats_available: req.body.business_seats_available
+            
+        }
+    );
+    flight.save().then((result)=>{
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(result, null, 4));
+    }).catch((err)=>
+    {
+        res.status(400).send(err);
+    });
+};
+
+const listFlights=(req,res)=>
+{
+    Flight.find().then((result)=>{
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(result, null, 4));
+    });
+};
+const showFlight=(req,res)=>
+{
+    Flight.findById(req.params.id).then((result)=>{
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(result, null, 4));
+    });
+};
+
+
+const deleteFlight=(req,res)=>
+{
+    Flight.findByIdAndRemove(req.params.id, req.body)
+    .then(result => res.json({ mgs: 'Book entry deleted successfully' }))
+    .catch(err => res.status(404).json({ error: 'No such a flight' }));
+};
+
+const updateFlight=(req,res)=>
+{
+    var id=req.params.id;
+    console.log(req.params);
+    Flight.findOne({_id:id}).then((result)=>{
+        if(req.body.flight_number)
+        {
+            result.flight_number=req.body.flight_number;
+        }
+        if(req.body.arrival_time)
+        {
+            result.arrival_time=req.body.arrival_time;
+        }
+        if(req.body.departure_time)
+        {
+            result.departure_time=req.body.departure_time;
+        }
+        if(req.body.from)
+        {
+            result.from=req.body.from;
+        }
+        if(req.body.to)
+        {
+            result.to=req.body.to;
+        }
+        if(req.body.flight_date)
+        {
+            result.flight_date=req.body.flight_date;
+        }
+        if(req.body.economy_seats_available)
+        {
+            result.economy_seats_available=req.body.economy_seats_available;
+        }
+        if(req.body.business_seats_available)
+        {
+            result.business_seats_available=req.body.business_seats_available;
+        }
+
+
+
+        result.save().then((result)=>
+        {
+            res.send("update is done");
+        }).catch((err)=>
+        {
+            console.log(err);
+        })
+    });
+};
+
+
+
+// const getAllStudents=(req,res)=>
+// {
+//     const neededJob='student';
+//     User.find({Job:neededJob}).then((result)=>{
+//         res.header("Content-Type",'application/json');
+//         res.send(JSON.stringify(result, null, 4));
+//     });
+// };
+
+// const getAllSuperheroes=(req,res)=>
+// {
+//     const neededJob='Superhero';
+//     User.find({Job:neededJob}).then((result)=>{
+//         res.header("Content-Type",'application/json');
+//         res.send(JSON.stringify(result, null, 4));
+//     });
+// };
+
+module.exports=
+{
+    createFlight,
+    listFlights,
+    deleteFlight,
+    updateFlight,
+    showFlight
+    
+}
---
+++ Backend/Routes/FlightRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const userController=require('../Controller/UserController');
+const flightRouter=express.Router();
+flightRouter.use(express.json());
+flightRouter.use(express.urlencoded({extended: false}));
+
+
+
+flightRouter.get('/',userController.listFlights);
+
+flightRouter.get('/:id',userController.showFlight);
+
+flightRouter.put('/update/:id',userController.updateFlight);
+
+flightRouter.delete('/delete/:id',userController.deleteFlight);
+
+flightRouter.post('/create',userController.createFlight);
+
+module.exports=flightRouter;
