413e92598303670905654fd2c66ba07c190a2374
Watidy <mostafaelwatidy@hotmail.com>
2021-12-01 16:49:08 +0200

    added return flights to user search results

--- Frontend/react-app/src/Components/UserSearchFlights.js
+++ Frontend/react-app/src/Components/UserSearchFlights.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router";
+//import { useLocation } from "react-router";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -51,9 +51,67 @@ const UserSearchFlights = (props) => {
               <p className="left-txt">
                 {" "}
                 <Typography>
+                <b>Flight Number: </b> {flight.flight_number}{" "}
+                
                 <p className="left-txt">
                 {" "}
+                <br/>
+                <b> From: </b> {flight.from } {flight.departure_time}{" "}
+              </p>
+              <p className="left-txt">
+                {" "}
+                <b>To: </b> {flight.from } {flight.arrival_time}{" "}
+              </p>
+                
+              <p className="left-txt">
+                {" "}
+                <b>Price: </b> {flight.price}{" "}
+              </p>
+                </Typography>
+              </p>
+            </AccordionSummary>
+            <AccordionDetails>
+            <Typography>
+            <p className="left-txt">
+                {" "}
                 <b>Flight Date: </b> {flight.flight_date}{" "}
+              </p>
+              <p className="left-txt">
+                {" "}
+                <b>Economy: </b> {flight.economy_seats_available}{" "}
+              </p>
+              <p className="left-txt">
+                {" "}
+                <b>Business: </b> {flight.business_seats_available}
+              </p>
+            </Typography>
+            </AccordionDetails>
+            </Accordion>
+           
+
+          </li>
+          
+        ))}
+      </ul>
+    
+
+      <ul>
+      <h3>Return Flights</h3>
+        {returnFlights.map((flight) => (
+          
+          <li key={flight._id}>
+            <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header">
+            
+              <p className="left-txt">
+                {" "}
+                <Typography>
+                <b>Flight Date: </b> {flight.flight_date}{" "}
+                <p className="left-txt">
+                {" "}
               </p>
                 
                 
